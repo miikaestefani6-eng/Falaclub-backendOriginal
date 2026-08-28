b01488f46ee529ba93779ee9d55a3cbc938a0326
miikaestefani6-eng <miikaestefani6@icloud.com>
2026-08-28 16:42:49 -0300

    chore: register protected management routes

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -10,6 +10,11 @@ import { Route as FlashcardsRouteImport } from './routes/flashcards'
 import { Route as DropsRouteImport } from './routes/drops'
 import { Route as ProgressoRouteImport } from './routes/progresso'
 import { Route as OnboardingRouteImport } from './routes/onboarding'
+import { Route as PerfilRouteImport } from './routes/perfil'
+import { Route as MasterRouteImport } from './routes/master'
+import { Route as EscolaRouteImport } from './routes/escola'
+import { Route as ProfessorRouteImport } from './routes/professor'
+import { Route as CreatorRouteImport } from './routes/creator'
 
 const IndexRoute = IndexRouteImport.update({ id: '/', path: '/', getParentRoute: () => rootRouteImport } as any)
 const LoginRoute = LoginRouteImport.update({ id: '/login', path: '/login', getParentRoute: () => rootRouteImport } as any)
@@ -20,13 +25,18 @@ const FlashcardsRoute = FlashcardsRouteImport.update({ id: '/flashcards', path: 
 const DropsRoute = DropsRouteImport.update({ id: '/drops', path: '/drops', getParentRoute: () => rootRouteImport } as any)
 const ProgressoRoute = ProgressoRouteImport.update({ id: '/progresso', path: '/progresso', getParentRoute: () => rootRouteImport } as any)
 const OnboardingRoute = OnboardingRouteImport.update({ id: '/onboarding', path: '/onboarding', getParentRoute: () => rootRouteImport } as any)
+const PerfilRoute = PerfilRouteImport.update({ id: '/perfil', path: '/perfil', getParentRoute: () => rootRouteImport } as any)
+const MasterRoute = MasterRouteImport.update({ id: '/master', path: '/master', getParentRoute: () => rootRouteImport } as any)
+const EscolaRoute = EscolaRouteImport.update({ id: '/escola', path: '/escola', getParentRoute: () => rootRouteImport } as any)
+const ProfessorRoute = ProfessorRouteImport.update({ id: '/professor', path: '/professor', getParentRoute: () => rootRouteImport } as any)
+const CreatorRoute = CreatorRouteImport.update({ id: '/creator', path: '/creator', getParentRoute: () => rootRouteImport } as any)
 
-export interface FileRoutesByFullPath { '/': typeof IndexRoute; '/login': typeof LoginRoute; '/mia': typeof MiaRoute; '/hoje': typeof HojeRoute; '/biblioteca': typeof BibliotecaRoute; '/flashcards': typeof FlashcardsRoute; '/drops': typeof DropsRoute; '/progresso': typeof ProgressoRoute; '/onboarding': typeof OnboardingRoute }
+export interface FileRoutesByFullPath { '/': typeof IndexRoute; '/login': typeof LoginRoute; '/mia': typeof MiaRoute; '/hoje': typeof HojeRoute; '/biblioteca': typeof BibliotecaRoute; '/flashcards': typeof FlashcardsRoute; '/drops': typeof DropsRoute; '/progresso': typeof ProgressoRoute; '/onboarding': typeof OnboardingRoute; '/perfil': typeof PerfilRoute; '/master': typeof MasterRoute; '/escola': typeof EscolaRoute; '/professor': typeof ProfessorRoute; '/creator': typeof CreatorRoute }
 export interface FileRoutesByTo extends FileRoutesByFullPath {}
-export interface FileRoutesById { __root__: typeof rootRouteImport; '/': typeof IndexRoute; '/login': typeof LoginRoute; '/mia': typeof MiaRoute; '/hoje': typeof HojeRoute; '/biblioteca': typeof BibliotecaRoute; '/flashcards': typeof FlashcardsRoute; '/drops': typeof DropsRoute; '/progresso': typeof ProgressoRoute; '/onboarding': typeof OnboardingRoute }
+export interface FileRoutesById { __root__: typeof rootRouteImport; '/': typeof IndexRoute; '/login': typeof LoginRoute; '/mia': typeof MiaRoute; '/hoje': typeof HojeRoute; '/biblioteca': typeof BibliotecaRoute; '/flashcards': typeof FlashcardsRoute; '/drops': typeof DropsRoute; '/progresso': typeof ProgressoRoute; '/onboarding': typeof OnboardingRoute; '/perfil': typeof PerfilRoute; '/master': typeof MasterRoute; '/escola': typeof EscolaRoute; '/professor': typeof ProfessorRoute; '/creator': typeof CreatorRoute }
 export interface FileRouteTypes { fileRoutesByFullPath: FileRoutesByFullPath; fullPaths: keyof FileRoutesByFullPath; fileRoutesByTo: FileRoutesByTo; to: keyof FileRoutesByFullPath; id: keyof FileRoutesById; fileRoutesById: FileRoutesById }
-export interface RootRouteChildren { IndexRoute: typeof IndexRoute; LoginRoute: typeof LoginRoute; MiaRoute: typeof MiaRoute; HojeRoute: typeof HojeRoute; BibliotecaRoute: typeof BibliotecaRoute; FlashcardsRoute: typeof FlashcardsRoute; DropsRoute: typeof DropsRoute; ProgressoRoute: typeof ProgressoRoute; OnboardingRoute: typeof OnboardingRoute }
-const rootRouteChildren: RootRouteChildren = { IndexRoute, LoginRoute, MiaRoute, HojeRoute, BibliotecaRoute, FlashcardsRoute, DropsRoute, ProgressoRoute, OnboardingRoute }
+export interface RootRouteChildren { IndexRoute: typeof IndexRoute; LoginRoute: typeof LoginRoute; MiaRoute: typeof MiaRoute; HojeRoute: typeof HojeRoute; BibliotecaRoute: typeof BibliotecaRoute; FlashcardsRoute: typeof FlashcardsRoute; DropsRoute: typeof DropsRoute; ProgressoRoute: typeof ProgressoRoute; OnboardingRoute: typeof OnboardingRoute; PerfilRoute: typeof PerfilRoute; MasterRoute: typeof MasterRoute; EscolaRoute: typeof EscolaRoute; ProfessorRoute: typeof ProfessorRoute; CreatorRoute: typeof CreatorRoute }
+const rootRouteChildren: RootRouteChildren = { IndexRoute, LoginRoute, MiaRoute, HojeRoute, BibliotecaRoute, FlashcardsRoute, DropsRoute, ProgressoRoute, OnboardingRoute, PerfilRoute, MasterRoute, EscolaRoute, ProfessorRoute, CreatorRoute }
 export const routeTree = rootRouteImport._addFileChildren(rootRouteChildren)._addFileTypes<FileRouteTypes>()
 import type { getRouter } from './router.tsx'
 import type { startInstance } from './start.ts'
